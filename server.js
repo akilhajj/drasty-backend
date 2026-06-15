f7a13a7730a6210673bbdf7872377efed9592828
akilhajj <akilhajj00@gmail.com>
2026-06-15 10:09:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 const CLOUDFLARE_ACCOUNT_ID = process.env.CLOUDFLARE_ACCOUNT_ID;
 const CLOUDFLARE_DATABASE_ID = process.env.CLOUDFLARE_DATABASE_ID;
